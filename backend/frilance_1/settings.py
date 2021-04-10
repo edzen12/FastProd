cb77c42795ccb9991f1fc48658ae9eb398ec7771
edzen12 <oichiev.edzen@gmail.com>
2021-04-10 17:40:29 +0600

    edit staticfiles

--- backend/frilance_1/settings.py
+++ backend/frilance_1/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
